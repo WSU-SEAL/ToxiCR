1de2cdd266cab232e6bb8290e85911e1cdc8f1cc
Alexander Goussas <goussasalexander@gmail.com>
2023-11-12 12:52:31 -0500

    Update test to use latest pre-trained model parameters

--- ToxiCRTest.py
+++ ToxiCRTest.py
@@ -51,28 +51,35 @@ from ToxiCR import ToxiCR
 #                        embedding="fasttext", load_pretrained=True)
 
 # Bert, the best performing model
-toxicClassifier=ToxiCR(ALGO="BERT", count_profanity=False, remove_keywords=False, split_identifier=False,
-                       embedding="bert", load_pretrained=True)
+toxicClassifier = ToxiCR(
+    ALGO="BERT",
+    count_profanity=False,
+    remove_keywords=True,
+    split_identifier=False,
+    embedding="tfidf",
+    load_pretrained=True
+)
 
 toxicClassifier.init_predictor()
 
-
-
-sentences=["go fuck yourself",
-           "this is crap",
-           "thank you for the information",
-           "yeah that sucked, fixed, Done.",
-           "Crap, this is an artifact of a previous revision. It's simply the last time a change was made to Tuskar's cloud.",
-           "Ah damn I misread the bug -_-",
-           "wtf...",
-           "I appreciate your help.",
-           "fuuuuck",
-           "what the f*ck",
-           "absolute shit",
-           "Get the hell outta here",
-            "shi*tty code",
-           "you are an absolute b!tch",
-           "Nothing particular to worry about"]
+sentences = [
+    "go fuck yourself",
+    "this is crap",
+    "thank you for the information",
+    "yeah that sucked, fixed, Done.",
+    "Crap, this is an artifact of a previous revision. It's simply the last time a change was made to Tuskar's cloud.",
+    "Ah damn I misread the bug -_-",
+    "wtf...",
+    "I appreciate your help.",
+    "fuuuuck",
+    "what the f*ck",
+    "absolute shit",
+    "Get the hell outta here",
+     "shi*tty code",
+    "you are an absolute b!tch",
+    "Nothing particular to worry about",
+    "You need to kill the process for it to work"
+]
 
 
 results = toxicClassifier.get_toxicity_probability(sentences)
